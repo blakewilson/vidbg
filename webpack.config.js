f24d28dbe9950db66b935cc34c5c7deec0ce0f34
Blake Wilson <blake@pushlabs.co>
2019-04-02 12:53:34 -0700

    Make NPM scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 const path = require('path')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/vidbg.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'vidbg.js',
-    library: 'vidbg',
-    libraryTarget: 'var'
-  },
-  devtool: 'none',
-  module: {
-    rules: [
-      {
-        use: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }
-    ]
-  },
-  plugins: []
+module.exports = env => {
+  return {
+    mode: env || 'production',
+    entry: './src/vidbg.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'vidbg.js',
+      library: 'vidbg',
+      libraryTarget: 'var'
+    },
+    devtool: 'none',
+    module: {
+      rules: [
+        {
+          use: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: []
+  }
 }
